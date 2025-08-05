f6134d5aac3b911640c84ded42f75dbb7505e7e0
AllanSantanna <allanxlink@hotmail.com>
2025-08-05 11:14:49 -0300

    feat: user test

--- src/entities/user/user.test.ts
+++ src/entities/user/user.test.ts
@@ -1,71 +1,86 @@
 import { User } from './index'
 
+const now = new Date()
+
 describe('it should verify user entity when created', () => {
     test('if user is valid', () => {
-        const userDev = User.create({
+        const userDevEither = User.create({
             email: 'user@dev.com',
             husbandName: 'Marido Dev',
             wifeName: 'Mulher Dev',
             password: 'Us3r_D3v',
         })
 
-        expect(userDev.isRight()).toBe(true)
-    })
-    test('if user with email: dev@test.a is not a valid user', () => {
-        const userDev = User.create({
-            email: 'dev@test.a',
-            husbandName: 'Marido Dev',
-            wifeName: 'Mulher Dev',
-            password: 'Us3r_D3v',
-        })
-        expect(userDev.isLeft()).toBe(true)
+        if (userDevEither.isLeft()) {
+            throw new Error(
+                `Error creating user: ${userDevEither.error.message}`,
+            )
+        }
+
+        const userDev = userDevEither.value
+        const {
+            id,
+            email,
+            husbandName,
+            wifeName,
+            password,
+            createAt,
+            updateAt,
+        } = userDev.serialize()
+
+        expect(userDevEither.isRight()).toBe(true)
+        expect(id).toBeNull()
+        expect(email).toBe('user@dev.com')
+        expect(husbandName).toBe('Marido Dev')
+        expect(wifeName).toBe('Mulher Dev')
+        expect(password).toBe('Us3r_D3v')
+        expect(createAt).toBeInstanceOf(Date)
+        expect(updateAt).toBeInstanceOf(Date)
     })
 
-    test('if user with husbandName invalid is not a valid user', () => {
-        const userDev = User.create({
-            email: 'dev@test.com',
-            husbandName: '',
-            wifeName: 'Mulher Dev',
-            password: 'Us3r_D3v',
-        })
-        expect(userDev.isLeft()).toBe(true)
-    })
+    test('if invalid fields return errors', () => {
+        const invalidCases = [
+            {
+                email: 'dev@test.a',
+                husbandName: 'Marido Dev',
+                wifeName: 'Mulher Dev',
+                password: 'Us3r_D3v',
+            },
+            {
+                email: 'dev@test.com',
+                husbandName: '',
+                wifeName: 'Mulher Dev',
+                password: 'Us3r_D3v',
+            },
+            {
+                email: 'dev@test.com',
+                husbandName: 'Marido Dev',
+                wifeName: '',
+                password: 'Us3r_D3v',
+            },
+            {
+                email: 'dev@test.com',
+                husbandName: 'Marido Dev',
+                wifeName: 'Mulher Dev',
+                password: '123456',
+            },
+            {
+                email: 'dev@test.a',
+                husbandName: 'Marido Dev',
+                wifeName: 'Mulher Dev',
+                password: '',
+            },
+        ]
 
-    test('if user with wifeName invalid is not a valid user', () => {
-        const userDev = User.create({
-            email: 'dev@test.com',
-            husbandName: 'Marido Dev',
-            wifeName: '',
-            password: 'Us3r_D3v',
-        })
-        expect(userDev.isLeft()).toBe(true)
-    })
-
-    test('if user with password: 123456 is not a valid user', () => {
-        const userDev = User.create({
-            email: 'dev@test.a',
-            husbandName: 'Marido Dev',
-            wifeName: 'Mulher Dev',
-            password: '123456',
-        })
-        expect(userDev.isLeft()).toBe(true)
-    })
-
-    test('if user without password is not a valid user', () => {
-        const userDev = User.create({
-            email: 'dev@test.a',
-            husbandName: 'Marido Dev',
-            wifeName: 'Mulher Dev',
-            // @ts-expect-error test
-            password: null,
-        })
-        expect(userDev.isLeft()).toBe(true)
+        for (const input of invalidCases) {
+            const userDev = User.create(input)
+            expect(userDev.isLeft()).toBe(true)
+        }
     })
 })
 
 describe('it should verify user entity when loaded', () => {
     test('if user is valid when load', () => {
-        const now = new Date()
         const userDev = User.load({
             id: 1,
             email: 'user@dev.com',
@@ -76,41 +91,62 @@ describe('it should verify user entity when loaded', () => {
             updateAt: now,
         })
 
+        if (userDev.isLeft()) {
+            throw new Error(`Error loading user: ${userDev.error.message}`)
+        }
+
+        const user = userDev.value
+        const {
+            id,
+            email,
+            husbandName,
+            wifeName,
+            password,
+            createAt,
+            updateAt,
+        } = user.serialize()
+
         expect(userDev.isRight()).toBe(true)
+        expect(id).toBe(1)
+        expect(email).toBe('user@dev.com')
+        expect(husbandName).toBe('Marido Dev')
+        expect(wifeName).toBe('Mulher Dev')
+        expect(password).toBe('Us3r_D3v')
+        expect(createAt).toBeInstanceOf(Date)
+        expect(updateAt).toBeInstanceOf(Date)
     })
 
-    test('if load user without id is not a valid', () => {
-        const now = new Date()
-        const userDev = User.load({
-            // @ts-expect-error test
-            id: null,
-            email: 'dev@test.a',
-            husbandName: 'Marido Dev',
-            wifeName: 'Mulher Dev',
-            password: 'S3nh@F0rt3',
-            createAt: now,
-            updateAt: now,
-        })
-        expect(userDev.isLeft()).toBe(true)
-    })
+    test('if invalid fields return errors on load', () => {
+        const invalidCases = [
+            {
+                id: -1,
+                email: 'dev@test.a',
+                husbandName: 'Marido Dev',
+                wifeName: 'Mulher Dev',
+                password: 'S3nh@F0rt3',
+                createAt: now,
+                updateAt: now,
+            },
+            {
+                id: 1,
+                email: 'dev@test.com',
+                husbandName: '',
+                wifeName: 'Mulher Dev',
+                password: 'Us3r_D3v',
+                createAt: now,
+                updateAt: now,
+            },
+        ]
 
-    test('if load user with husbandName invalid is not a valid', () => {
-        const now = new Date()
-        const userDev = User.load({
-            id: 1,
-            email: 'dev@test.com',
-            husbandName: '',
-            wifeName: 'Mulher Dev',
-            password: 'Us3r_D3v',
-            createAt: now,
-            updateAt: now,
-        })
-        expect(userDev.isLeft()).toBe(true)
+        for (const input of invalidCases) {
+            const userDev = User.load(input)
+            expect(userDev.isLeft()).toBe(true)
+        }
     })
 })
 
 describe('it should verify user entity when changed', () => {
-    test('if user is valid when email is chenged', () => {
+    test('if user is valid when email is changed', () => {
         const userDevEither = User.create({
             email: 'user@dev.com',
             husbandName: 'Marido Dev',
@@ -119,20 +155,21 @@ describe('it should verify user entity when changed', () => {
         })
 
         if (userDevEither.isLeft())
-            return expect(userDevEither.isLeft()).not.toBe(true)
+            throw new Error(
+                `Error creating user: ${userDevEither.error.message}`,
+            )
 
         const userDev = userDevEither.value
         const newEmail = 'user@dev.com.br'
 
         const eitherChanged = userDev.changeEmail(newEmail)
-
         const { email } = userDev.serialize()
 
         expect(email).toBe(newEmail)
         expect(eitherChanged.isRight()).toBe(true)
     })
 
-    test('if the email exchange is invalid if the new email is invalid', () => {
+    test('if email is invalid when changed', () => {
         const userDevEither = User.create({
             email: 'user@dev.com',
             husbandName: 'Marido Dev',
@@ -141,13 +178,14 @@ describe('it should verify user entity when changed', () => {
         })
 
         if (userDevEither.isLeft())
-            return expect(userDevEither.isLeft()).not.toBe(true)
+            throw new Error(
+                `Error creating user: ${userDevEither.error.message}`,
+            )
 
         const userDev = userDevEither.value
         const newEmail = 'userdev.com.br'
 
         const eitherChanged = userDev.changeEmail(newEmail)
-
         expect(eitherChanged.isLeft()).toBe(true)
     })
 })
